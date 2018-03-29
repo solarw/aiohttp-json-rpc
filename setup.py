9dda76f0a7978ac70f00c3102291dcec1494f08c
Yuri (solarw) Turchenkov <solarw@solarw.info>
2018-03-29 16:57:00 +0300

    version updated

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 __name__ == '__main__' and setup(name='aiohttp-json-rpc',
-      version='0.9',
-      author='Florian Scherf',
-      url='https://github.com/pengutronix/aiohttp-json-rpc/',
-      author_email='f.scherf@pengutronix.de',
-      license='Apache 2.0',
-      install_requires=['aiohttp>=3,<3.1'],
-      python_requires='>=3.5',
-      packages=find_packages(),
-      zip_safe=False,
-      entry_points={
-          'pytest11': [
-              'aiohttp-json-rpc = aiohttp_json_rpc.pytest',
-          ]
-     })
+                                 version='0.9.1-slw1',
+                                 author='Florian Scherf',
+                                 url='https://github.com/pengutronix/aiohttp-json-rpc/',
+                                 author_email='f.scherf@pengutronix.de',
+                                 license='Apache 2.0',
+                                 install_requires=['aiohttp>=3,<3.1'],
+                                 python_requires='>=3.5',
+                                 packages=find_packages(),
+                                 zip_safe=False,
+                                 entry_points={
+                                     'pytest11': [
+                                         'aiohttp-json-rpc = aiohttp_json_rpc.pytest',
+                                     ]
+                                 })
